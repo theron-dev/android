7a23e381951ab3ec1113e70cb922b3b3656f36d4
zhang hailong <hailongz.hit@gmail.com>
2014-07-27 17:33:19 +0800

    update

--- org.hailong.service/src/org/hailong/service/impl/UplinkService.java
+++ org.hailong.service/src/org/hailong/service/impl/UplinkService.java
@@ -1,11 +1,20 @@
 package org.hailong.service.impl;
 
+import java.lang.reflect.Method;
+
 import org.hailong.service.AbstractService;
 import org.hailong.service.IServiceContext;
+import org.hailong.service.ITask;
+import org.hailong.service.S;
+import org.hailong.service.task.impl.APITask;
+import org.hailong.service.tasks.IAPICancelTask;
+import org.hailong.service.tasks.IAPIResponseTask;
 import org.hailong.service.tasks.IUplinkTask;
 import org.hailong.service.tasks.IUplinkTaskListener;
 
-public abstract class UplinkService<ST extends IServiceContext> extends AbstractService<ST> {
+import android.util.Log;
+
+public class UplinkService<ST extends IServiceContext> extends AbstractService<ST> {
 
 	public void doDidUplinkTaskLoaded(Class<?> taskType,IUplinkTask uplinkTask,Object resultsData){
 		IUplinkTaskListener listener = uplinkTask.getListener();
@@ -19,6 +28,70 @@ public abstract class UplinkService<ST extends IServiceContext> extends Abstract
 		if(listener != null){
 			listener.onDidUnlinkTaskException(taskType, uplinkTask, exception);
 		}
+	}
+
+	@Override
+	public <T extends ITask> boolean handle(Class<T> taskType, T task,
+			int priority) throws Exception {
+		
+		if(IAPIResponseTask.class == taskType){
+			
+			String name = "handle" + taskType.getSimpleName() + "Response";
+			
+			Method method = null;
+			
+			try{
+				method = this.getClass().getMethod(name, IServiceContext.class,Class.class,ITask.class,IAPIResponseTask.class);
+			}
+			catch(Exception ex){
+				Log.d(S.TAG, Log.getStackTraceString(ex));
+			}
+			
+			if(method != null){
+				return (Boolean) method.invoke(getContext(), taskType,task,(IAPIResponseTask) task);
+			}
+			
+		}
+		else {
+			
+			String name = "handle" + taskType.getSimpleName();
+			
+			Method method = null;
+			
+			try{
+				method = this.getClass().getMethod(name, IServiceContext.class,Class.class,ITask.class,int.class);
+			}
+			catch(Exception ex){
+				Log.d(S.TAG, Log.getStackTraceString(ex));
+			}
+			
+			if(method != null){
+				return (Boolean) method.invoke(getContext(), taskType,task,priority);
+			}
+			
+		}
+		
+		return false;
+	}
+
+	@Override
+	public <T extends ITask> boolean cancelHandle(Class<T> taskType, T task)
+			throws Exception {
+		
+		APITask cancelTask = new APITask();
+		
+		cancelTask.setTaskType(taskType);
+		cancelTask.setTask(task);
+		
+		getContext().handle(IAPICancelTask.class, cancelTask, 0);
+		
+		return false;
+	}
+
+	@Override
+	public boolean cancelHandleForSource(Object source) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 
